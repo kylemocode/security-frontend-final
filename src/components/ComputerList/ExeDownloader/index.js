97cb41b487c4d02694c34dc360668bd1a5a2a45f
Oldmo <35811214+105306006@users.noreply.github.com>
2019-08-21 16:53:35 +0800

    computerlist page complete

--- src/components/ComputerList/ExeDownloader/index.js
+++ src/components/ComputerList/ExeDownloader/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { ExeContainer, ExeImage } from './style';
+import { ExeContainer, ExeImage, DownloadBtn } from './style';
 
 const ExeDownloader = () => {
     return (
         <ExeContainer>
             <ExeImage src="https://s3-ap-northeast-1.amazonaws.com/www.memepr.com/smartQA/Security+Rabbit+logo+design_white_logo_text.png "/>
-            <p style={{marginTop:'-50px'}}>test</p>
+            <p style={{marginTop:'-60px',fontFamily:'微軟正黑體',fontWeight:'550',fontSize:'15px'}}>下載 Security Rabbit 的應用程式來掃描電腦中的可執行檔，用戶可以自行選擇掃描方式與範圍，立即取得大至整間企業小至一個執行檔的安全狀態與詳細資訊。</p>
+            <DownloadBtn>Download</DownloadBtn>
         </ExeContainer>
     )
 }
--- src/components/ComputerList/ExeDownloader/style.js
+++ src/components/ComputerList/ExeDownloader/style.js
@@ -11,10 +11,25 @@ export const ExeContainer = styled.div`
     flex-direction: column;
     align-items: center;
     margin-top: 10px;
+    position: relative;
 `
 
 export const ExeImage = styled.img`
     width: 100%;
     margin-top: -80px;
     transform: scale(0.7);
+`
+export const DownloadBtn = styled.button`
+    border-radius: 5px;
+    border: solid 1px rgb(48,62,143);
+    color: rgb(48,62,143);
+    transition: 0.3s;
+    background-color: transparent;
+    font-weight: 600;
+    padding: 5px 10px;
+    box-sizing: border-box;
+    :hover {
+        background-color: rgb(48,62,143);
+        color: white;
+    }
 `
