21a0e9a464e6a855d79c23f240c536f979c23674
Oldmo <35811214+105306006@users.noreply.github.com>
2019-08-22 19:10:18 +0800

    ui fixed

--- src/components/ComputerList/CompanyTotalScore/index.js
+++ src/components/ComputerList/CompanyTotalScore/index.js
@@ -46,30 +46,30 @@ const CompanyTotalScore = (props) => {
             
             <div style={{display:'flex',justifyContent:'space-between',alignItems:'center',marginTop: '30px'}}>
                 <StatusLabel>
-                    <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',fontFamily:'微軟正黑體',fontWeight:'700',color:'rgba(0,0,0,0.8)',fontSize:'14px',marginTop:'3px',padding:'2px 0px'}}>
+                    <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',fontFamily:'微軟正黑體',fontWeight:'700',color:'rgba(0,0,0,0.8)',fontSize:'14px',marginTop:'3px',padding:'18px 0px'}}>
                         <div style={flexCenter}>
                             <StatusIcon bgc="#55ed6c"/>
-                            <p style={{margin:'0',marginLeft:'7px'}}>安全狀態 <span>(0-3)</span></p>
+                            <p style={{margin:'0',marginLeft:'7px',fontSize:'16px'}}>安全狀態 <span>(0-3)</span></p>
                         </div>
                         <p style={{margin:'0px'}}>電腦安全性高</p>
                         <p>安全狀態電腦數量: {props.statusNum.safeNum}</p>
                     </div>
                 </StatusLabel>
                 <StatusLabel>
-                    <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',fontFamily:'微軟正黑體',fontWeight:'700',color:'rgba(0,0,0,0.8)',fontSize:'14px',marginTop:'3px',padding:'2px 0px'}}>
+                    <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',fontFamily:'微軟正黑體',fontWeight:'700',color:'rgba(0,0,0,0.8)',fontSize:'14px',marginTop:'3px',padding:'18px 0px'}}>
                         <div style={flexCenter}>
                             <StatusIcon bgc="#f0a33e"/>
-                            <p style={{margin:'0',marginLeft:'7px'}}>警告狀態 <span>(4-6)</span></p>
+                            <p style={{margin:'0',marginLeft:'7px',fontSize:'16px'}}>警告狀態 <span>(4-6)</span></p>
                         </div>
                         <p style={{margin:'0px'}}>電腦可能存在潛在威脅</p>
                         <p>警告狀態電腦數量: {props.statusNum.warningNum}</p>
                      </div>
                 </StatusLabel>
                 <StatusLabel>
-                    <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',fontFamily:'微軟正黑體',fontWeight:'700',color:'rgba(0,0,0,0.8)',fontSize:'14px',marginTop:'3px',padding:'2px 0px'}}>
+                    <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',fontFamily:'微軟正黑體',fontWeight:'700',color:'rgba(0,0,0,0.8)',fontSize:'14px',marginTop:'3px',padding:'18px 0px'}}>
                         <div style={flexCenter}>
                             <StatusIcon bgc="#de351f"/>
-                            <p style={{margin:'0',marginLeft:'7px'}}>危險狀態 <span>(7-9)</span></p>
+                            <p style={{margin:'0',marginLeft:'7px',fontSize:'16px'}}>危險狀態 <span>(7-9)</span></p>
                         </div>
                     <p style={{margin:'0px'}}>電腦存在危險</p>
                     <p>危險狀態電腦數量: {props.statusNum.dangerousNum}</p>
--- src/components/ComputerList/ExeDownloader/index.js
+++ src/components/ComputerList/ExeDownloader/index.js
@@ -3,7 +3,7 @@ import { ExeContainer, ExeImage, DownloadBtn } from './style';
 
 const ExeDownloader = () => {
     return (
-        <ExeContainer>
+        <ExeContainer style={{paddingBottom:'50px'}}>
             <div style={{flex:'2.8'}}>
                 <h3 style={{fontWeight:'600',fontSize:'22px',marginTop:'12px',marginBottom:'15px'}}>立即下載 Security Rabbit !</h3>
                 <p style={{fontWeight:'300',fontSize:'14px',color:'rgba(255,255,255,0.7)',marginBottom:'30px'}}>下載 Security Rabbit 應用程式來掃描電腦中的可執行檔，自行選擇掃描方式與範圍，立即取得大至整間企業小至一個執行檔的安全狀態與詳細資訊。</p>
@@ -11,7 +11,7 @@ const ExeDownloader = () => {
                 
             </div>
             <div style={{flex:'2'}}>
-                <img src="https://s3-ap-northeast-1.amazonaws.com/www.memepr.com/smartQA/undraw_download_pc33.svg" style={{width:'200px',height:'210px',position:'absolute',bottom:'5px',right:'2px'}}/>
+                <img src="https://s3-ap-northeast-1.amazonaws.com/www.memepr.com/smartQA/Group+56.png" style={{height:'230px',position:'absolute',bottom:'0px',right:'15px'}}/>
             </div>
         </ExeContainer>
     )
