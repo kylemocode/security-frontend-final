31c98dc182ff8582a0a2bb1dc680b406bed165b5
oldmo860617 <oldmo860617@gmail.com>
2019-09-29 17:20:42 +0800

    0

--- src/components/FileUpload/UploadInput/index.js
+++ src/components/FileUpload/UploadInput/index.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import { InputContainer, InputLogo, StyledInput, InputOuterContainer } from './style';
+import React, {useState} from 'react'
+import { InputContainer, InputLogo, StyledInput, InputOuterContainer, InputLabel, UploadButton, FilenameLabel } from './style';
 
 const UploadInput = () => {
+
+	const [uploadFile, setUploadFile] = useState({})
+	const [isUpload, setIsUpload] = useState(false);
+
+	const handleSubmit = (e) => {
+		if(!isUpload) {
+			alert('尚未選擇檔案');
+		}
+		e.preventDefault();
+		const data = new FormData();
+		data.append('file', uploadFile)
+
+		// 呼叫 api
+	};
+
+	const onChangeHandler = (e) => {
+		setIsUpload(true);
+		setUploadFile(e.target.files[0]);
+		console.log(e.target.files[0]);
+	};
+
 	return (
 		<InputContainer>
 			<InputLogo src="https://cdn1.iconfinder.com/data/icons/hawcons/32/699751-icon-105-document-file-exe-512.png"/>
 			<InputOuterContainer>
-				<StyledInput type="file" name="upload"/>
+				<form onSubmit={handleSubmit} style={{display:'flex',flexDirection:'column',alignItems: 'center'}}>
+					<InputLabel>
+						<StyledInput type="file" name="file" onChange={onChangeHandler}/>
+						選擇檔案
+					</InputLabel>
+					{ isUpload ? <FilenameLabel>{uploadFile.name}</FilenameLabel> : ""}
+					<UploadButton>上傳</UploadButton>
+				</form>
 			</InputOuterContainer>
 		</InputContainer>
 	)
--- src/components/FileUpload/UploadInput/style.js
+++ src/components/FileUpload/UploadInput/style.js
@@ -15,11 +15,41 @@ export const InputLogo = styled.img`
 	height: 100px;
 `
 export const InputOuterContainer = styled.div`
+	width: 250px;
 	border: solid 1px rgba(48,62,143,0.2);
 	padding: 30px 50px;
 	min-height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: white;
+	border-radius: 5px;
 `
 
 export const StyledInput = styled.input`
-	width: 250px;
+	
+	display: none;
+`
+
+export const InputLabel = styled.label`
+	color: white;
+	height: 30px;
+	border-radius: 4px;
+	background-color: rgb(48,62,143);
+	padding: 5px 15px;
+	font-size: 13px;
+	cursor: pointer;
+`
+
+export const UploadButton = styled.button`
+	border: solid 1px rgb(48,62,143);
+	background-color: transparent;
+	color: rgb(48,62,143);
+	font-size: 10px;
+	width: 40px;
+`
+
+export const FilenameLabel = styled.p`
+	color: rgba(0,0,0,0.7);
+	font-size: 11px;
 `
